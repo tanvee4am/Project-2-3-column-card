de6cdd15648bf8174c1104d1fe8d12b83ee9bd27
tanvee4am <tanvee4am@gmail.com>
2024-09-25 18:25:53 +0500

    init repo

---
+++ index.js
@@ -0,0 +1,32 @@
+// do { 
+//     const numbers = [];
+//     let number = prompt("Enter a number");
+//     number = Number,parseInt(number);
+//     numbers.push(number);
+// } while (a != 0);
+
+// console.log(document);
+// console.log("asdasd");
+// console.log({abb:"aa", cc:"ss"});
+
+// document.getElementById()
+// document.getElementsByClassName()
+
+// let vv = document.querySelector(".first").classList.add("second")
+// var hh = document.querySelectorAll(".first")
+
+// function asdf() {
+//     document.querySelector(".heading").style = "font-size: 30px"
+// }
+
+// document.querySelector(".secondbutton").addEventListener("click", function() {
+//     document.querySelectorAll(".heading")[1].style = "font-size: 30px"
+// })
+// fetch()
+
+html forms
+JSON
+dom
+async Js
+fetch api
+localStorage 
